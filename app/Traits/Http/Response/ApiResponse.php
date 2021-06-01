c2d2a1b1ed5467cf8568dba6862bdf09be9c877b
Angel Recinos <migueln6@gmail.com>
2021-06-01 15:07:06 -0600

    apiresponse trait.

---
+++ app/Traits/Http/Response/ApiResponse.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace App\Traits\Http\Response;
+
+
+use App\Http\Controllers\Api\ErrorCodes;
+
+trait ApiResponse
+{
+    protected $code = 0;
+    protected $message = '';
+    protected $httpCode = '200';
+
+    public function setCode($code){
+        $this->code =$code;
+    }
+
+    public function setMessage($message){
+        $this->message = $message;
+    }
+
+    public function setHttpCode($httpCode){
+        $this->httpCode = $httpCode;
+    }
+
+    public function setResponseParams($code, $message, $httpCode){
+        $this->code = $code;
+        $this->message = $message;
+        $this->httpCode = $httpCode;
+
+    }
+
+    public function setNotFoundResponse(){
+        $this->setResponseParams(ErrorCodes::NOT_FOUND,'not found',404);
+    }
+
+    public function setUnauthenticatedResponse(){
+        $this->setResponseParams(ErrorCodes::UNAUTHENTICATED, 'unauthenticated',401);
+    }
+
+    public function setUnauthorizedResponse(){
+        $this->setResponseParams(ErrorCodes::UNAUTHORIZED,'unauthorized',403);
+    }
+
+    public function setInvalidInputResponse(){
+        $this->setResponseParams(ErrorCodes::INVALID_INPUT,'invalid input', 422);
+    }
+
+    public function setInvalidAssignationResponse() {
+        $this->setResponseParams(ErrorCodes::INVALID_ASSIGNATION, 'invalid assignation', 422);
+    }
+
+    public function setInternalErrorResponse(){
+        $this->setResponseParams(ErrorCodes::INTERNAL_ERROR, 'internal error', 500);
+    }
+
+    /**
+     * @param array $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function apiResponse($data=[]): \Illuminate\Http\JsonResponse
+    {
+        $data["code"]= $this->code;
+        $data["message"]= $this->message;
+        return response()->json($data,$this->httpCode);
+    }
+
+}
