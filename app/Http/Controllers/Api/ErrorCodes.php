c56b7aee6eb8ad0fa4eb1f99c0736dbb7f0636fa
Angel Recinos <migueln6@gmail.com>
2021-06-01 15:06:55 -0600

    api common error codes.

---
+++ app/Http/Controllers/Api/ErrorCodes.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+
+class ErrorCodes
+{
+    const NOT_FOUND=1;
+    const UNAUTHENTICATED=2;
+    const UNAUTHORIZED=3;
+    const INVALID_INPUT=4;
+    const INVALID_ASSIGNATION=5;
+    const INTERNAL_ERROR=6;
+}
