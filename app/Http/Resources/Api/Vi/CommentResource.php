6aab21f0b66398f7cba1a91d98e91efa894beb2e
Angel Recinos <migueln6@gmail.com>
2021-06-01 16:26:35 -0600

    Resource for Comment

---
+++ app/Http/Resources/Api/Vi/CommentResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Api\Vi;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'author' => $this->author,
+            'email' => $this->email,
+            'comment' => $this->comment,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
