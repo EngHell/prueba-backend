8f505374d7aa1513197b930ccad4283730802e34
Angel Recinos <migueln6@gmail.com>
2021-06-01 15:33:20 -0600

    added a comment model.

---
+++ database/migrations/2021_06_01_211825_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',25)->nullable(false)->change();
+            $table->string('author',255)->nullable(false)->change();
+            $table->string('email',255)->nullable(false)->change();
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
