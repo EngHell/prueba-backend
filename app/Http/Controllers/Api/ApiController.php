640f546d486759ff55da1100641a7ecf0fa62cbb
Angel Recinos <migueln6@gmail.com>
2021-06-01 15:07:25 -0600

    added ApiController.php

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Http\Controllers\Controller;
+use App\Traits\Http\Response\ApiResponse;
+
+class ApiController extends Controller
+{
+
+    use ApiResponse;
+}
