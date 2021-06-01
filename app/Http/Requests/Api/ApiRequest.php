6bab8331c928e26c0a5823ab2b904640f275238d
Angel Recinos <migueln6@gmail.com>
2021-06-01 16:38:40 -0600

    added custom ApiRequestBase

---
+++ app/Http/Requests/Api/ApiRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Requests\Api;
+
+
+use App\Traits\Http\Request\AlwaysExpectsJsonAndWantsJson;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class ApiRequest extends Request
+{
+    use AlwaysExpectsJsonAndWantsJson;
+}
