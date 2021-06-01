b8a8559986e83fa8c5e98c52a063e87b8886cc71
Angel Recinos <migueln6@gmail.com>
2021-06-01 17:43:21 -0600

    created StoreCommentRequest.php

---
+++ app/Http/Requests/Api/Vi/StoreCommentRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Api\Vi;
+
+use App\Http\Requests\Api\ApiFormRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'=>[
+                'required',
+                'max:255'
+            ],
+            'email'=>[
+                'required',
+                'email',
+                'max:255'
+            ],
+            'author'=>[
+                'required',
+                'max:255'
+            ],
+            'comment'=>[
+                'required',
+                'max:65535'
+            ],
+        ];
+    }
+}
