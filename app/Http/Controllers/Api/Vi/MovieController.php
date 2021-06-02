9ecbc0c4105a3948f8c2291b10e3564a16c97a97
Angel Recinos <migueln6@gmail.com>
2021-06-01 19:22:27 -0600

    removed uneeded code.

--- app/Http/Controllers/Api/Vi/MovieController.php
+++ app/Http/Controllers/Api/Vi/MovieController.php
@@ -11,27 +11,6 @@ use Illuminate\Support\Facades\Http;
 class MovieController extends ApiController
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
